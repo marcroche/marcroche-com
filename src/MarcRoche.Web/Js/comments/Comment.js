e09a2c968824a2c073cc3b932e7f12b1877723d2
marcroche <marcroche@triad.rr.com>
2014-04-02 22:43:56 -0400

    converting comments section to angular

---
+++ src/MarcRoche.Web/Js/comments/Comment.js
@@ -0,0 +1,15 @@
+﻿var marcroche_blog = marcroche_blog || {};
+
+marcroche_blog.Comment = function () {
+    this.author;
+    this.email;
+    this.content;
+
+    this.isValid = function () {
+        return validate(this.author) && validate(this.email) && validate(this.content);
+    };
+
+    function validate(field) {
+        return field !== undefined && field !== null && field.length > 0;
+    }
+};
---
+++ src/MarcRoche.Web/Js/comments/commentsController.js
@@ -0,0 +1,18 @@
+﻿app.controller("CommentsCtrl", function ($scope) {
+    var comment = new marcroche_blog.Comment();
+    $scope.message = "This is some angular shit";
+
+    $scope.newComment = comment;
+
+    $scope.newCommentIsInvalid = true;
+
+    $scope.createComment = function (form) {
+
+        if (form.$valid === false) {
+            $scope.newCommentIsInvalid = true;
+        }
+
+        console.log("Creating comment");
+        console.log("Author: " + $scope.newComment.author);
+    };
+}); 
